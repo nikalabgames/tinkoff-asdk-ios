5551d7a1175afacbc94a1cc59ac7cda1dca64caf
Andrey Balakin <2411003+nikalabgames@users.noreply.github.com>
2025-03-16 12:12:29 +0300

    Update IUIApplication.swift

--- TinkoffASDKUI/TinkoffASDKUI/Common/Protocols/IUIApplication.swift
+++ TinkoffASDKUI/TinkoffASDKUI/Common/Protocols/IUIApplication.swift
@@ -10,11 +10,11 @@ import UIKit
 protocol IUIApplication {
     func canOpenURL(_ url: URL) -> Bool
 
-    func open(_ url: URL, options: [UIApplication.OpenExternalURLOptionsKey: Any], completionHandler completion: ((Bool) -> Void)?)
+    func open(_ url: URL, completionHandler completion: ((Bool) -> Void)?)
 }
 
 extension IUIApplication {
-    func open(_ url: URL, options: [UIApplication.OpenExternalURLOptionsKey: Any], completionHandler completion: ((Bool) -> Void)?) {
+    func open(_ url: URL, completionHandler completion: ((Bool) -> Void)?) {
         open(url, options: [:], completionHandler: completion)
     }
 }
